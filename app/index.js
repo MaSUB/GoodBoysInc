1ae54b5bf48b3dc2ade017de77f2c252be126393
Michael <pro585g@gmail.com>
2017-03-04 01:14:01 -0500

    moved things and changed index

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var reload =require('reload');
 var app = express();
 
 var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
@@ -10,7 +11,7 @@ var personality_insights = new PersonalityInsightsV3({
 
 var params = {
   // Get the content items from the JSON file.
-  content_items: require('./profile.json').contentItems,
+  content_items: require('./data/profile.json').contentItems,
   consumption_preferences: true,
   raw_scores: true,
   headers: {
@@ -30,10 +31,15 @@ personality_insights.profile(params, function(error, response) {
   }
 );
 
+
+
 app.get('/', function(req, res) {
-  res.send('<h1>Roux Academy Meetups</h1>');
+  res.send('<h1>Roux Academy Meetups</h1>
+            <h2></h2>');
 });
 
 var server = app.listen(3000, function() {
   console.log('Listening on port 3000');
 });
+
+reload(server, app);
