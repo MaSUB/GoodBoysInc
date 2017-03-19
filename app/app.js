f8d03b18e78a5806f3611c64bf77acf4afa33757
Mike Stumpf <michaelstumpf585@gmail.com>
2017-03-19 19:43:36 +0000

    trying to print to file, got jquery post to work

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var reload = require('reload');
 var app = express();
+var bodyParser = require('body-parser');
 
 //var dataFile = require('./data/data.json');
 
@@ -11,11 +12,14 @@ app.set('views', 'app/views');
 
 app.locals.siteTitle = 'Good Boys Inc';
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/personality'));
 app.use(require('./routes/twitter'));
 app.use(require('./routes/facebook'));
+
 
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
--- app/routes/facebook.js
+++ app/routes/facebook.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var jsonfile = require("jsonfile");
 var session = require('client-sessions');
 
 router.get('/facebook', function(req, res) {
@@ -8,4 +9,16 @@ router.get('/facebook', function(req, res) {
     pageID: 'facebook'
   });
 });
+
+router.post('/facebook/info', function(req, res){
+	var obj = {asdf : "asdf"};
+	var file = '/../data/faceFeed.json';
+      jsonfile.writeFile(file, obj, function(err){
+        console.error(err);
+      });
+	//console.log('body: ' + JSON.stringify(req.body));
+	res.send(req.body);
+});
+
 module.exports = router;
+
