0ebb2c948050c56cb533be3b1588c397eb01b6e4
Mike Stumpf <michaelstumpf585@gmail.com>
2017-04-19 20:22:27 +0000

    added set report need to mess with it

--- app/routes/account.js
+++ app/routes/account.js
@@ -18,7 +18,34 @@ function getReport_callback(req, res, err){
     var obj = {
       "error": "no report"
     };
+    res.send(obj);
+  }
+}
+
+function setReport_callback(req, res, err){
+  
+  if(err){
     
+    res.send("server side error");
+  }else if(req.body != null){
+    
+    /* Write obj to file */
+    //var file = './../GoodBoysInc/app/data/facefeed.json';
+  	var file = '/home/ubuntu/workspace/app/data/faceFeed.json';
+    jsonfile.writeFile(file, req.body, function(err){
+       if(err){
+         console.error(err);
+       }
+    });
+    
+    // do something with this 
+    res.send("got it");
+    
+  }else{
+    
+    var obj = {
+      "error": "no report"
+    };
     res.send(obj);
   }
 }
@@ -43,6 +70,7 @@ router.post('/account/getReport', function(req, res) {
 
 router.post('/account/setReport', function(req, res) {
 
- db.set_report(req, res, getReport_callback);
+ db.set_report(req, res, setReport_callback);
 });
+
 module.exports = router;
